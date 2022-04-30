d936dff55e83f878a16bac2903459dc8ddc52219
ztf <undefine4080@gmail.com>
2022-04-30 10:31:55 +0800

    perfor: Input 与label绑定

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -3,23 +3,24 @@ import commonProps from "../../base/types/commonInterface";
 import './input.less';
 
 interface propsInput extends commonProps {
-    type: HTMLInputTypeAttribute,
+    type?: HTMLInputTypeAttribute,
     label?: string,
     placeholder?: string,
+    name?: string,
     onChange?: ChangeEventHandler<HTMLInputElement>,
 }
 
-const Input: React.FC<propsInput> = (props) => {
+const Input: React.FC<propsInput> = ( props ) => {
     const PREFIX = 'wdu-input';
-    const { size = "normal", label, type = 'text', placeholder, width, onChange, value } = props;
-    const componentStyle = `${PREFIX}-container ${PREFIX}-${size}`;
+    const { id, size = "normal", name, label, type = 'text', placeholder, onChange } = props;
+    const classList = `${ PREFIX }-container ${ PREFIX }-${ size }`;
 
     return (
-        <div className={componentStyle}>
+        <div className={ classList }>
             {
-                label?.toString().length && <label htmlFor="" className={`${PREFIX}-label`}>{label?.toString()}</label>
+                label?.toString().length && <label htmlFor={ id } className={ `${ PREFIX }-label` }>{ label?.toString() }</label>
             }
-            <input type={type} defaultValue={typeof value === "number" ? Number(value) : String(value)} className={`${PREFIX}-input`} placeholder={placeholder?.toString()} onChange={onChange} style={{ width }} />
+            <input id={ id } type={ type } name={ name } className={ `${ PREFIX }-input` } placeholder={ placeholder?.toString() } onChange={ onChange } />
         </div>
     );
 };
--- src/introFragments/introInput.tsx
+++ src/introFragments/introInput.tsx
@@ -2,35 +2,36 @@ import { IntroComponent } from "../base/introComponent";
 import { Input } from "../components/Input/Input";
 
 export default function () {
+    const formName = "personInfo";
     const props = {
         title: 'Input 输入框',
         position: 'input',
         components: [
             {
-                component: <Input label="姓名" type=" text" size="small" />,
+                component: <Input name={ formName } label="姓名" size="small" />,
                 info: 'small 输入框'
             },
             {
-                component: <Input label="密码" type="password" size="normal" />,
+                component: <Input name={ formName } label="密码" type="password" size="normal" />,
                 info: 'normal 输入框(默认)'
             },
             {
-                component: <Input label="年龄" type="number" size="large" />,
+                component: <Input name={ formName } label="年龄" type="number" size="large" />,
                 info: 'large 输入框'
             },
             {
-                component: <Input label="住址" type="tel" size="normal" placeholder="蒙古大草原" value={250} />,
+                component: <Input name={ formName } label="住址" type="tel" size="normal" placeholder="xxx路xxx号" />,
                 info: '占位内容'
             },
         ],
         code: `
         import { Input } from "../components/Input/Input";
 
-        <Input label="姓名" type="text" size="small" />
-        <Input label="密码" type="password" size="normal" />
-        <Input label="年龄" type="number" size="large" />
+        <Input name="personInfo" label="姓名" size="small" />
+        <Input name="personInfo" label="密码" type="password" size="normal" />
+        <Input name="personInfo" label="年龄" type="number" size="large" />
         `
     };
 
-    return <IntroComponent {...props} />;
+    return <IntroComponent { ...props } />;
 };
