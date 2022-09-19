f1dd098974a36cae29ef1076331c6fe12c5abd4c
ztf <undefine4080@gmail.com>
2022-09-19 10:31:13 +0800

    feat: upgrade React v18

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+
 import App from './App';
 
 import './index.css';
 
-ReactDOM.render(
+const container = document.getElementById( 'root' );
+const root = createRoot( ( container as HTMLElement ) );
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById( 'root' )
 );
