c51f96689748c39b3fcfe9e2a59134ef90a96950
ztf <undefine4080@gmail.com>
2022-09-25 19:46:00 +0800

    fix: Checkbox stopPropagation while click

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,28 +1,42 @@
-import React, { ChangeEventHandler } from "react";
+import { ChangeEvent } from "react";
 import commonProps from "../../base/types/commonInterface";
 import './checkbox.less';
 
 interface propsCheckbox extends commonProps {
-    label?: string; // 标签
-    name?: string; // 组名称
-    value?: string; // 代表值
-    onChange?: ChangeEventHandler<HTMLInputElement>;
+    label?: string;
+    name?: string;
+    checked?: boolean;
+    onChange?: ( value: boolean ) => any;
 }
 
 function Checkbox ( props: propsCheckbox ) {
     const PREFIX = 'wdu-checkbox';
-    const { label, name, value, id, onChange, disabled = false } = props;
+    const { label, name, checked = false, id, onChange, disabled = false } = props;
 
     const labelStyle = disabled ? `${ PREFIX }-label ${ PREFIX }-disabled` : `${ PREFIX }-label`;
     const checkboxStyle = disabled ? `${ PREFIX } wdu-no-event` : PREFIX;
     const checkmarkStyle = disabled ? `${ PREFIX }-checkmark ${ PREFIX }-disabled` : `${ PREFIX }-checkmark`;
 
+    const handleChange = ( e: ChangeEvent<any> ) => {
+        e.stopPropagation();
+        onChange && onChange( e.target.checked );
+    };
+
     return (
-        <div className={ `${ PREFIX }-container` } >
+        <div className={ `${ PREFIX }-container` }
+            onClick={ ( e: any ) => e.stopPropagation() }
+        >
             <label htmlFor="" className={ labelStyle }>
                 { label?.toString() }
 
-                <input type="checkbox" id={ id } name={ name } value={ value } className={ checkboxStyle } onChange={ onChange } />
+                <input
+                    type="checkbox"
+                    id={ id }
+                    name={ name }
+                    checked={ checked }
+                    className={ checkboxStyle }
+                    onChange={ handleChange }
+                />
 
                 <span className={ checkmarkStyle }></span>
             </label>
