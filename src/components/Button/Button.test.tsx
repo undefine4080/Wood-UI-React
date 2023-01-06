bf0fb305075a68dee1c98b4e64a81720ee87546a
zhengtengfei <zhengtengfei@hanwang.com.cn>
2023-01-06 14:31:04 +0800

    test: Button

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,60 +1,56 @@
 import { render, fireEvent } from '@testing-library/react';
-import { type } from 'os';
 import { describe, test, expect, vi } from 'vitest';
 
 import Button from "./Button";
 import { propsButton } from './type';
 
-describe( 'Button', () => {
-    test( 'button click event', () => {
+describe('Button', () => {
+    test('button click event', () => {
         const handleCallback = vi.fn();
-        const button = render( <Button onClick={ handleCallback }></Button> );
-        const element = button.getByRole( 'button' );
-        fireEvent.click( element );
-        expect( handleCallback ).toHaveBeenCalled();
-    } );
+        const button = render(<Button onClick={handleCallback}></Button>);
+        const element = button.getByRole('button');
+        fireEvent.click(element);
+        expect(handleCallback).toHaveBeenCalled();
+    });
 
-    test( 'disable the button', () => {
+    test('disable the button', () => {
         const handleClick = vi.fn();
-        const button = render( <Button onClick={ handleClick } disabled></Button> );
-        const element = button.getByRole( 'button' );
-        fireEvent.click( element );
-        expect( handleClick ).not.toHaveBeenCalled();
-    } );
+        const button = render(<Button onClick={handleClick} disabled></Button>);
+        const element = button.getByRole('button');
+        fireEvent.click(element);
+        expect(handleClick).not.toHaveBeenCalled();
+    });
 
-    test( 'the loading button', () => {
-        const button = render( <Button loading></Button> );
-        const buttonHTML = button.container.firstElementChild;
-        expect( buttonHTML?.firstElementChild?.outerHTML ).toBe( '<i class="wdu-icon-loading"></i>' );
-    } );
-} );
+    test('the loading button', () => {
+        const { container } = render(<Button loading>loading Button</Button>);
+        expect(container).toMatchSnapshot();
+    });
+});
 
 const defineTypes = [
-    { type: 'plain', expected: 'wdu-button-plain' },
-    { type: 'important', expected: 'wdu-button-important' },
-    { type: 'danger', expected: 'wdu-button-danger' },
-    { type: 'success', expected: 'wdu-button-success' },
-    { type: 'border', expected: 'wdu-button-border' },
-    { type: 'warn', expected: 'wdu-button-warn' },
-    { type: 'line', expected: 'wdu-button-line' }
+    { type: 'plain' },
+    { type: 'important' },
+    { type: 'danger' },
+    { type: 'success' },
+    { type: 'border' },
+    { type: 'warn' },
+    { type: 'line' }
 ];
-describe.each( defineTypes )( 'test the type of button', ( { type, expected } ) => {
-    test( `returns ${ expected }`, () => {
-        const button = render( <Button type={ ( type as propsButton[ 'type' ] ) }></Button> );
-        const element = button.getByRole( 'button' );
-        expect( element.classList.contains( expected ) ).toBeTruthy();
-    } );
-} );
+describe.each(defineTypes)('test the type of button', ({ type }) => {
+    test(`${type} type`, () => {
+        const { container } = render(<Button type={(type as propsButton['type'])}>{type} Button</Button>);
+        expect(container).toMatchSnapshot();
+    });
+});
 
 const defineSize = [
-    { size: 'small', expected: 'wdu-button-small' },
-    { size: 'normal', expected: 'wdu-button-normal' },
-    { size: 'large', expected: 'wdu-button-large' },
+    { size: 'small' },
+    { size: 'normal' },
+    { size: 'large' },
 ];
-describe.each( defineSize )( 'test the size of button', ( { size, expected } ) => {
-    test( `${ size } size button`, () => {
-        const button = render( <Button size={ ( size as propsButton[ 'size' ] ) }></Button> );
-        const element = button.getByRole( 'button' );
-        expect( element.classList.contains( expected ) ).toBeTruthy();
-    } );
-} );
+describe.each(defineSize)('test the size of button', ({ size }) => {
+    test(`${size} size`, () => {
+        const { container } = render(<Button size={(size as propsButton['size'])}>{size} Button</Button>);
+        expect(container).toMatchSnapshot();
+    });
+});
