2ea8a73ac615e51cbb1784408d9d39a6f7a32b83
zhengtengfei <zhengtengfei@hanwang.com.cn>
2023-01-13 17:38:07 +0800

    doc: use hashRouter

--- dist/assets/tsMode.7f6d41a9.js
+++ dist/assets/tsMode.7f6d41a9.js
@@ -1,4 +1,4 @@
-var M=Object.defineProperty;var K=(e,t,r)=>t in e?M(e,t,{enumerable:!0,configurable:!0,writable:!0,value:r}):e[t]=r;var _=(e,t,r)=>(K(e,typeof t!="symbol"?t+"":t,r),r);import{t as R,m as E}from"./index.77d0dfe3.js";/*!-----------------------------------------------------------------------------
+var M=Object.defineProperty;var K=(e,t,r)=>t in e?M(e,t,{enumerable:!0,configurable:!0,writable:!0,value:r}):e[t]=r;var _=(e,t,r)=>(K(e,typeof t!="symbol"?t+"":t,r),r);import{t as R,m as E}from"./index.e8a864c9.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.34.1(547870b6881302c5b4ff32173c16d06009e3588f)
  * Released under the MIT license
