f92eaa94a994fcdd420b6fce1caba6bb7d91f738
ztf <undefine4080@gmail.com>
2023-04-24 22:16:42 +0800

    doc: Popover

--- dist/assets/razor.78eff77e.js
+++ dist/assets/razor.78eff77e.js
@@ -1,4 +1,4 @@
-import{m as s}from"./index.ebbb70d9.js";/*!-----------------------------------------------------------------------------
+import{m as s}from"./index.1e76c7a5.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.34.1(547870b6881302c5b4ff32173c16d06009e3588f)
  * Released under the MIT license
--- src/introFragments/IntroPopover.tsx
+++ src/introFragments/IntroPopover.tsx
@@ -2,8 +2,33 @@ import { IntroComponent } from '../base/components/IntroComponent';
 import Popover from '@component/Popover/Popover';
 import { propsIntroComponent } from '@base/types/commonInterface';
 import Button from '@component/Button/Button';
+import { Table, TableColumn } from '@component/Table/Table';
 
 const ex = 'this is a popover';
+
+const testData = [
+    {
+        fruit: '苹果',
+        color: '红色',
+        weight: 3478,
+        productArea: '陕西',
+        date: '2020-04-07',
+    },
+    {
+        fruit: '香蕉',
+        color: '黄色',
+        weight: 1267,
+        productArea: '海南',
+        date: '2021-05-27',
+    },
+    {
+        fruit: '荔枝',
+        color: '红色',
+        weight: 8087,
+        productArea: '广东',
+        date: '2022-01-23',
+    },
+];
 
 export default function () {
     const props: propsIntroComponent = {
@@ -49,12 +74,32 @@ export default function () {
                 info: '',
                 code: '',
             },
-            // {
-            //     title: '自定义内容',
-            //     component: <Popover content={ex}></Popover>,
-            //     info: '',
-            //     code: '',
-            // },
+            {
+                title: '自定义内容',
+                component: (
+                    <>
+                        <Popover
+                            content={
+                                <Table
+                                    data={testData}
+                                    title='去年购买水果情况一览表'>
+                                    <TableColumn prop='fruit' label='水果' />
+                                    <TableColumn prop='color' label='颜色' />
+                                    <TableColumn prop='weight' label='重量' />
+                                    <TableColumn
+                                        prop='productArea'
+                                        label='产地'
+                                    />
+                                    <TableColumn prop='date' label='日期' />
+                                </Table>
+                            }>
+                            <Button>自定义内容</Button>
+                        </Popover>
+                    </>
+                ),
+                info: '',
+                code: '',
+            },
         ],
     };
 
