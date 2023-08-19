49423a7ef270f4d6a7f32b8ef5e72420dc2fde5e
ztf <undefine4080@gmail.com>
2023-08-19 11:01:04 +0800

    chore: build config update

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,53 +1,25 @@
-import Tooltip from "@component/Tooltip/Tooltip";
-import Tag from "@component/Tag/Tag";
-import Button from "@component/Button/Button";
-import Image from "@component/Image/Image";
-import Input from "@component/Input/Input";
-import Modal from "@component//Modal/Modal";
-import Radio from "@component/Radio/Radio";
-import Switch from "@component/Switch/Switch";
-import Tree from "@component/Tree/Tree";
-import Checkbox from "@component/Checkbox/Checkbox";
-import Divider from "@component/Divider/Divider";
-import Mark from "@component/Mark/Mark";
-import Message from "@component/Message/Message";
-import Pagination from "@component/Pagination/Pagination";
+import Tooltip from '@component/Tooltip/Tooltip';
+import Tag from '@component/Tag/Tag';
+import Button from '@component/Button/Button';
+import Image from '@component/Image/Image';
+import Input from '@component/Input/Input';
+import Modal from '@component//Modal/Modal';
+import Radio from '@component/Radio/Radio';
+import Switch from '@component/Switch/Switch';
+import Tree from '@component/Tree/Tree';
+import Checkbox from '@component/Checkbox/Checkbox';
+import Divider from '@component/Divider/Divider';
+import Mark from '@component/Mark/Mark';
+import Message from '@component/Message/Message';
+import Pagination from '@component/Pagination/Pagination';
 import {
-  Collapse,
-  CollapseItem,
-  CollapseNav,
-} from "@component/Collapse/Collapse";
-import { Carousel, CarouselItem } from "@component/Carousel/Carousel";
-import { Form, FormItem } from "@component/Form/Form";
-import {
-  Container,
-  Aside,
-  Header,
-  Footer,
-  Main,
-  Row,
-  Col,
-} from "@component/Layout/Layout";
-// import { Nav, NavItem, NavMenu } from "@component/Nav/Nav";
-import { Select, Option } from "@component/Select/Select";
-import { Tab, TabPane } from "@component/Tab/Tab";
-import { Table, TableColumn } from "@component/Table/Table";
-import { NavMenu, NavMenuItem, SubNavMenu } from "@component/NavMenu/NavMenu";
-
-declare namespace woodUiReact {
-  export {
-    Button,
     Collapse,
     CollapseItem,
     CollapseNav,
-    Carousel,
-    CarouselItem,
-    Checkbox,
-    Divider,
-    Form,
-    FormItem,
-    Image,
-    Input,
+} from '@component/Collapse/Collapse';
+import { Carousel, CarouselItem } from '@component/Carousel/Carousel';
+import { Form, FormItem } from '@component/Form/Form';
+import {
     Container,
     Aside,
     Header,
@@ -55,25 +27,51 @@ declare namespace woodUiReact {
     Main,
     Row,
     Col,
-    Mark,
-    Message,
-    Modal,
-    Pagination,
-    Radio,
-    Switch,
-    Tree,
-    NavMenu,
-    SubNavMenu,
-    NavMenuItem,
-    Select,
-    Option,
-    Tab,
-    TabPane,
-    Table,
-    TableColumn,
-    Tag,
-    Tooltip,
-  };
-}
+} from '@component/Layout/Layout';
+// import { Nav, NavItem, NavMenu } from "@component/Nav/Nav";
+import { Select, Option } from '@component/Select/Select';
+import { Tab, TabPane } from '@component/Tab/Tab';
+import { Table, TableColumn } from '@component/Table/Table';
+import { NavMenu, NavMenuItem, SubNavMenu } from '@component/NavMenu/NavMenu';
 
-export = woodUiReact;
+declare module 'wood-ui' {
+    export {
+        Button,
+        Collapse,
+        CollapseItem,
+        CollapseNav,
+        Carousel,
+        CarouselItem,
+        Checkbox,
+        Divider,
+        Form,
+        FormItem,
+        Image,
+        Input,
+        Container,
+        Aside,
+        Header,
+        Footer,
+        Main,
+        Row,
+        Col,
+        Mark,
+        Message,
+        Modal,
+        Pagination,
+        Radio,
+        Switch,
+        Tree,
+        NavMenu,
+        SubNavMenu,
+        NavMenuItem,
+        Select,
+        Option,
+        Tab,
+        TabPane,
+        Table,
+        TableColumn,
+        Tag,
+        Tooltip,
+    };
+}
