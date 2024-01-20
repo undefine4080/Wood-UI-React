51b9dda3fa1445150ea4b2b7260f17e6a8bf18bb
ztf <undefine4080@gmail.com>
2024-01-20 17:31:04 +0800

    doc: doc build

--- vite.config.ts
+++ vite.config.ts
@@ -23,26 +23,6 @@ export default defineConfig({
         globals: true,
         environment: 'jsdom',
     },
-    build: {
-        lib: {
-            entry: resolve(__dirname, 'src/index.js'),
-            name: 'wood-ui-react',
-            fileName: (format) => `wood-ui.${format}.js`,
-        },
-        rollupOptions: {
-            // 确保外部化处理那些你不想打包进库的依赖
-            external: ['react', 'react/jsx-runtime'],
-            output: {
-                // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-                globals: {
-                    react: 'React',
-                },
-            },
-        },
-        minify: 'esbuild',
-        emptyOutDir: true,
-        outDir: 'build',
-    },
     css: {
         preprocessorOptions: {
             less: {
